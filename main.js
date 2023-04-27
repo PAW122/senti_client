c0149147fe419c961d3c50d17b9ced2060df6947
PAW122 <70094237+PAW122@users.noreply.github.com>
2023-04-27 22:49:37 +0200

    update 0.0.1
    basics

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-function senti_client() {
-console.log("Hello World")
+const axios = require("axios");
+
+async function senti_client(token) {
+    return connect(token)
 }
 
 module.exports = senti_client;
+
+async function connect(token) {
+
+    if(!token) {
+        throw new Error("token is undefind")
+    }
+
+    const DISCORD_API_URL = 'https://discord.com/api/v9';
+    const response = await axios.get(`${DISCORD_API_URL}/users/@me`, {
+        headers: {
+            Authorization: `Bot ${token}`,
+        },
+    });
+    return response.data;
+}
